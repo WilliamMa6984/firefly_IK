1c7a6a3364e8a149be83285ca0f838912a8f79b5
WM <williamm1849@gmail.com>
2024-06-26 13:35:53 +1000

    300*gamma for angle RMSE; evaluate firefly with both distance and angle RMSE

--- firefly_algorithm.py
+++ firefly_algorithm.py
@@ -44,28 +44,26 @@ class Firefly():
         d = self.euclid_dist(targetTf)
         RMSE = self.angle_dist(targetTf)
 
-        # self.intensity = 1 / (1 + gamma*d)
-        self.intensity = 0.5 / (1 + gamma*d) + 0.5 / (1 + 100*gamma*RMSE)
+        self.intensity = 0.5 / (1 + gamma*d) + 0.5 / (1 + 300*gamma*RMSE)
 
     def compute_fkine(self):
         self.Tf = f_kine(self.position)
 
     def move(self, other, alpha, beta, gamma):
-        # """
-        
         diff = other.position - self.position
         d = np.linalg.norm(diff) # sqrt(sum(abs((self.__position - better_position))))
         # self.position = self.position + beta*diff + alpha*rand_angles(num_angles)
         self.position = self.position + beta*np.exp(-gamma*(d**2))*diff + alpha*rand_angles(num_angles)
 
-    # def random_walk(self, alpha, fine_grain_alpha=0.1):
-    #     self.position = self.position + rand_angles(num_angles)*alpha*fine_grain_alpha
+    # def random_walk(self, alpha):
+    #     self.position = self.position + rand_angles(num_angles)*alpha
 
 def rand_angles(n):
     return (np.random.rand(n)-0.5)*pi
 
 def firefly_IK(target_Tf, maxGenerations, n, debug=False, graph=False, alpha=0.05, beta=0.02, gamma=0.08):
-    y_out = []
+    d_out = []
+    angle_out = []
     alpha_inner = alpha
 
     # Generate initial population
@@ -93,7 +91,7 @@ def firefly_IK(target_Tf, maxGenerations, n, debug=False, graph=False, alpha=0.0
         ax2.set_ylim([-1000, 1000])
         line2, = ax2.plot([0], [0], 'o')
 
-    best_ff = [0,0] # [intensity, ff_index]
+    best_ff = None
     
     for i in range(n):
         fireflies[i].compute_I(target_Tf, gamma)
@@ -103,27 +101,27 @@ def firefly_IK(target_Tf, maxGenerations, n, debug=False, graph=False, alpha=0.0
             for j in range(n): # nlog(n) loop
                 r = np.sum((fireflies[j].position - fireflies[i].position)**2)
                 if (fireflies[i].intensity < fireflies[j].intensity*math.exp(-gamma*r)):
-                # if (fireflies[i].intensity < fireflies[j].intensity):
                     fireflies[i].move(fireflies[j], alpha_inner, beta, gamma)
-                    fireflies[i].compute_fkine()
-                    fireflies[i].compute_I(target_Tf, gamma)
+            fireflies[i].compute_fkine()
+            fireflies[i].compute_I(target_Tf, gamma)
 
         # Get current best firefly
         best_i = get_best(fireflies)
-        if (fireflies[best_i].intensity > best_ff[0]):
-            best_ff = [fireflies[best_i].intensity, best_i]
+        if (best_ff is None or fireflies[best_i].intensity > best_ff.intensity):
+            best_ff = fireflies[best_i]
 
         # Random walk the best firefly
-        # fireflies[best_i].random_walk(alpha)
-        # Tfs[best_i] = f_kine(fireflies[best_i].position)
-        # fireflies[best_i].compute_I(Tfs[best_i], target_Tf, gamma)
+        # fireflies[best_i].random_walk(alpha_inner)
+        # fireflies[best_i].compute_fkine()
+        # fireflies[best_i].compute_I(target_Tf, gamma)
 
         t = t + 1
-        alpha_inner = alpha_new(alpha_inner, maxGenerations)
+        alpha_inner = alpha_new(alpha_inner, maxGenerations) # NOTE: May result in premature convergence
         
         # Misc.
         if (graph):
-            y_out.append(fireflies[best_i].euclid_dist(target_Tf))
+            d_out.append(fireflies[best_i].euclid_dist(target_Tf))
+            angle_out.append(fireflies[best_i].angle_dist(target_Tf))
         if (debug and t % 4 == 0):
             print(fireflies[best_i].intensity)
             print(fireflies[best_i].position)
@@ -141,9 +139,9 @@ def firefly_IK(target_Tf, maxGenerations, n, debug=False, graph=False, alpha=0.0
 
     # Return value
     if (graph):
-        return y_out
+        return [d_out, angle_out]
     else:
-        return fireflies[best_ff[1]]
+        return best_ff
 
 def alpha_new(alpha, n):
     delta = 1 - (0.005/0.9)**(1/n)
@@ -168,31 +166,31 @@ def f_kine(angles):
     [                                                                                                                                                                                0,                                                                                                                                                                                                                                                                                     0,                                                                                                                                                                      0,                                                                                                                                                                                                                                                                        1]])
     return Tf
 
-    def DH2tform(th, d, A, al):
-        T = np.array([
-        [cos(th), -cos(al)*sin(th),  sin(al)*sin(th), A*cos(th)],
-        [sin(th),  cos(al)*cos(th), -sin(al)*cos(th), A*sin(th)],
-        [    0,          sin(al),          cos(al),         d],
-        [    0,                0,                0,         1]
-        ])
-        return T
+    # def DH2tform(th, d, A, al):
+    #     T = np.array([
+    #     [cos(th), -cos(al)*sin(th),  sin(al)*sin(th), A*cos(th)],
+    #     [sin(th),  cos(al)*cos(th), -sin(al)*cos(th), A*sin(th)],
+    #     [    0,          sin(al),          cos(al),         d],
+    #     [    0,                0,                0,         1]
+    #     ])
+    #     return T
     
-    l1 = 155
-    l2 = 220
-    l3 = 45
-    l4 = 175
-    l5 = 115
-    T1 = DH2tform(-th1, l1, 0, pi/2)
-    T2 = DH2tform(th2, 0, 0, -pi/2)
-    T3 = DH2tform(0, l2, 0, pi/2)
-    T4 = DH2tform(th3, 0, 0, -pi/2)
-    T5 = DH2tform(-th4, l3+l4, 0, pi/2)
-    T6 = DH2tform(pi/2+th5, 0, 0, -pi/2)
-    T7 = DH2tform(th6, l5, 0, 0)
+    # l1 = 155
+    # l2 = 220
+    # l3 = 45
+    # l4 = 175
+    # l5 = 115
+    # T1 = DH2tform(-th1, l1, 0, pi/2)
+    # T2 = DH2tform(th2, 0, 0, -pi/2)
+    # T3 = DH2tform(0, l2, 0, pi/2)
+    # T4 = DH2tform(th3, 0, 0, -pi/2)
+    # T5 = DH2tform(-th4, l3+l4, 0, pi/2)
+    # T6 = DH2tform(pi/2+th5, 0, 0, -pi/2)
+    # T7 = DH2tform(th6, l5, 0, 0)
 
-    Tf_out = T1 @ T2 @ T3 @ T4 @ T5 @ T6 @ T7
+    # Tf_out = T1 @ T2 @ T3 @ T4 @ T5 @ T6 @ T7
 
-    return Tf_out
+    # return Tf_out
 
 def finetune_FA_IK():
 
@@ -200,24 +198,26 @@ def finetune_FA_IK():
     n = 10
 
     # Search space
-    # alpha_s = [0.1, 0.01, 0.001, 0.0001]
-    # beta_s = [0.1, 0.01, 0.001, 0.0001]
-    # gamma_s = [0.1, 0.01, 0.001, 0.0001]
+    alpha_s = [0.1, 0.01, 0.001, 0.0001]
+    beta_s = [0.1, 0.01, 0.001, 0.0001]
+    gamma_s = [0.1, 0.01, 0.001, 0.0001]
     # alpha_s = [0.1]
     # beta_s = [0.1]
     # gamma_s = [0.0001, 0.00001, 0.000001, 0.0000001, 0]
-    alpha_s = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8]
-    beta_s = [0.5]
-    gamma_s = [0.00001]
+    # alpha_s = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8]
+    # beta_s = [0.5]
+    # gamma_s = [0.00001]
 
     search_space = list(it.product(alpha_s, beta_s, gamma_s))
 
-    best_pair = []
+    best_pair = [[],[]]
     best_d = None
+    best_ang_d = None
 
     for abg in search_space:
         n = 20
         avg_d = 0
+        avg_ang_d = 0
 
         for _ in range(n):
             alpha = abg[0]
@@ -229,17 +229,24 @@ def finetune_FA_IK():
             sln = firefly_IK(target_Tf, maxGenerations, n, alpha=alpha, beta=beta, gamma=gamma)
 
             avg_d = avg_d + sln.euclid_dist(target_Tf)
+            avg_ang_d = avg_ang_d + sln.angle_dist(target_Tf)
 
         avg_d = avg_d / n
+        avg_ang_d = avg_ang_d / n
         print("-----------------")
         print(abg)
         print(avg_d)
+        print(avg_ang_d)
         print("-----------------")
         if (best_d is None or avg_d < best_d):
-            best_pair = abg
+            best_pair[0] = abg
             best_d = avg_d
+        if (best_ang_d is None or avg_ang_d < best_ang_d):
+            best_pair[1] = abg
+            best_ang_d = avg_ang_d
 
     print("================================BEST")
+    print("Distance, angle")
     print(best_pair)
     print(best_d)
 
@@ -265,9 +272,11 @@ def graph_FA_IK():
     
     pool = mp.Pool()
     ans = pool.map(graph_task, np.ones(10)*maxGenerations)
-    
+
+    fig, ax = plt.subplots(2)
     for i in range(0,10):
-        plt.plot(x, ans[i], '-')
+        ax[0].plot(x, ans[i][0], '-')
+        ax[1].plot(x, ans[i][1], '-')
         
     plt.show()
     
@@ -291,6 +300,8 @@ def debug():
 
     print("Euclid distance is: ")
     print(sln.euclid_dist(target_Tf))
+    print("Angle distance is: ")
+    print(sln.angle_dist(target_Tf))
 
     print("Transform is: ")
     print(f_kine(sln.position))
@@ -306,6 +317,6 @@ def debug_profile():
 if __name__ == "__main__":
     # debug_profile()
     # debug()
-    # finetune_FA_IK()
-    graph_FA_IK()
+    finetune_FA_IK()
+    # graph_FA_IK()
     print("wait")
